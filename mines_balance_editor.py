f8c2d8d323bae0be79c724491dea1c1784f6d5cc
v81d <zv81dx@gmail.com>
2025-06-08 14:39:18 -0400

    Update mines_balance_editor.py

--- mines_balance_editor.py
+++ mines_balance_editor.py
@@ -1,6 +1,8 @@
+from jwt import decode
 import os
 import requests
-from uuid import UUID
+
+# from uuid import UUID
 from math import inf
 from dotenv import find_dotenv, load_dotenv, set_key
 
@@ -12,39 +14,39 @@ def clear():
     os.system("cls || clear")  # Simplified solution to clearing the console
 
 
-def validate_uuid(uuid):
-    try:
-        UUID(uuid)
-    except:
-        return False
-    return True
+# def validate_uuid(uuid):
+#     try:
+#         UUID(uuid)
+#     except:
+#         return False
+#     return True
 
 
-def get_leaderboard():
-    data = requests.get(
-        "https://www.siths-mathathon.com/api/retreiveLeaderboard"
-    ).json()
-    return data["leaderboard"]
+# def get_leaderboard():
+#     data = requests.get(
+#         "https://www.siths-mathathon.com/api/retreiveLeaderboard"
+#     ).json()
+#     return data["leaderboard"]
 
 
-def search():  # Search through the users
-    clear()
+# def search():  # Search through the users
+#     clear()
 
-    leaderboard = get_leaderboard()
-    list = ""
-    for item in leaderboard:  # Iterate through the list and create a user list
-        list += f"\n[{leaderboard.index(item) + 1}] {item['user_name']} - {item['uid']}"
-    print(f"Below is the list of discovered users in the database:\n{list}")
-    user = input("\n:: Select a user number from the list: ")
-    if user.isnumeric():  # Is the input a valid number?
-        if int(user) > len(leaderboard):  # Is the selected user in the list?
-            print(
-                "The provided input is not in the range of the list. The task been terminated."
-            )
-        return leaderboard[int(user) - 1]  # Return the user data
-    else:
-        print("The provided input is not a valid number. The task has been terminated.")
-        os._exit(1)
+#     leaderboard = get_leaderboard()
+#     list = ""
+#     for item in leaderboard:  # Iterate through the list and create a user list
+#         list += f"\n[{leaderboard.index(item) + 1}] {item['user_name']} - {item['uid']}"
+#     print(f"Below is the list of discovered users in the database:\n{list}")
+#     user = input("\n:: Select a user number from the list: ")
+#     if user.isnumeric():  # Is the input a valid number?
+#         if int(user) > len(leaderboard):  # Is the selected user in the list?
+#             print(
+#                 "The provided input is not in the range of the list. The task been terminated."
+#             )
+#         return leaderboard[int(user) - 1]  # Return the user data
+#     else:
+#         print("The provided input is not a valid number. The task has been terminated.")
+#         os._exit(1)
 
 
 def load_jwt():
@@ -78,11 +80,10 @@ def load_jwt():
         return "Bearer " + resp["access_token"]
 
 
-def send_req(uid, balance):
+def send_req(balance):
     # Make the request
     headers = {"Authorization": os.environ["jwt"]}
     body = {
-        "userId": uid,
         "balance": balance,
         "gameState": {},
     }
@@ -107,7 +108,7 @@ def send_req(uid, balance):
         ) == "n":
             return False
         else:
-            send_req(uid, balance)
+            send_req(balance)
     else:
         print("\n" + str(resp))
         if (
@@ -122,44 +123,66 @@ def send_req(uid, balance):
 while True:
     clear()
 
-    if (
-        input(":: [Y/n] Would you like to select a user from the leaderboard? ").lower()
-        != "n"
-    ):
-        user = search()
-        uid = user["uid"]
-    else:  # Allow the user to enter a user ID directly
-        clear()
-        if (
-            input(
-                ":: [a/B] Would you like to input a user ID or retrieve it from .env? "
-            ).lower()
-            == "a"
-        ):
-            uid = input(":: What is the user ID of the target? ")
-        else:
-            uid = os.environ[
-                "uid"
-            ]  # Retrieve the user ID directly as an environment variable
+    # if (
+    #     input(":: [Y/n] Would you like to select a user from the leaderboard? ").lower()
+    #     != "n"
+    # ):
+    #     user = search()
+    #     uid = user["uid"]
+    # else:  # Allow the user to enter a user ID directly
+    #     clear()
+    #     if (
+    #         input(
+    #             ":: [a/B] Would you like to input a user ID or retrieve it from .env? "
+    #         ).lower()
+    #         == "a"
+    #     ):
+    #         uid = input(":: What is the user ID of the target? ")
+    #     else:
+    #         uid = os.environ[
+    #             "uid"
+    #         ]  # Retrieve the user ID directly as an environment variable
 
-    if not validate_uuid(uid):  # Check if the user ID is a valid UUID
-        print(f"The user ID ({uid}) is invalid. The task has been terminated.")
-        os._exit(1)
+    # if not validate_uuid(uid):  # Check if the user ID is a valid UUID
+    #     print(f"The user ID ({uid}) is invalid. The task has been terminated.")
+    #     os._exit(1)
 
-    # Set a new balance
+    # # Set a new balance
+    # try:
+    #     name = user["user_name"]
+    # except (NameError, KeyError, TypeError):
+    #     name = uid
+
+    # Try to decode the JWT
     try:
-        name = user["user_name"]
-    except (NameError, KeyError, TypeError):
-        name = uid
-    balance = input(f":: Enter the new balance for {name}: ")
+        decoded_jwt = decode(
+            os.environ["jwt"].replace("Bearer ", ""),
+            options={"verify_signature": False},
+            algorithms=["HS256"],
+        )
+    except:
+        new_jwt = load_jwt()
+        os.environ["jwt"] = new_jwt  # Update the OS environment variable
+        set_key(dotenv_file, "jwt", new_jwt)  # Update the dotenv file with the new JWT
+        decoded_jwt = decode(
+            os.environ["jwt"].replace("Bearer ", ""),
+            options={"verify_signature": False},
+            algorithms=["HS256"],
+        )
+
+    balance = input(
+        f":: Enter a new balance for {decoded_jwt["user_metadata"]["email"]} ({decoded_jwt["user_metadata"]["name"]}): "
+    )
     if balance.isnumeric():
         balance = int(balance)
-    elif balance.lower() == "infinity":
+    elif balance.lower() == "inf":
         balance = inf
+    elif balance.lower() == "-inf":
+        balance = -inf
     else:
         print("The provided input is not a valid number. The task has been terminated.")
         os._exit(1)
 
     # Update the balance
-    if not send_req(uid, balance):
+    if not send_req(balance):
         break
